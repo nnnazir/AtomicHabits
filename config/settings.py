bf469ab23c277f185a660419d5f135ce6961c09c
nnnazir <137882477+nnnazir@users.noreply.github.com>
2024-05-02 16:34:12 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -181,8 +181,8 @@ CSRF_TRUSTED_ORIGINS = [
 PUBLISHABLE_KEY_STRIPE = os.getenv('PUBLISHABLE_KEY_STRIPE')
 SECRET_KEY_STRIPE = os.getenv('SECRET_KEY_STRIPE')
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
